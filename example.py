251c98e8660ccef08cbbf821294902930478eb8b
Stefan Jansen <shjansen@gmail.com>
2013-08-19 22:23:08 +0200

    Initial commit of v0.1

---
+++ example.py
@@ -0,0 +1,6 @@
+import pyPostcode as pca
+
+postcodeapi = pca.pyPostcodeApi('{YOUR_API_KEY}')
+result_street = postcodeapi.getaddress('1011AC') # use p6 search
+result_address = postcodeapi.getaddress('1011AC', 154) # use address search
+print result_street._data, result_address._data
