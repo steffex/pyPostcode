e2840441c860edbcb2e9860b7515f6dad09c38de
Stefan Jansen <shjansen@gmail.com>
2013-08-19 22:35:27 +0200

    Updated example

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-import pyPostcode as pca
+from pyPostcode import pyPostcodeApi
 
-postcodeapi = pca.pyPostcodeApi('{YOUR_API_KEY}')
+postcodeapi = pyPostcodeApi('{YOUR_API_KEY}')
 result_street = postcodeapi.getaddress('1011AC') # use p6 search
 result_address = postcodeapi.getaddress('1011AC', 154) # use address search
 print result_street._data, result_address._data
