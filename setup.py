b54347f4d7f48b508e3bbfb7e528eb595e9d7596
Stefan Rijnhart <stefan@therp.nl>
2015-02-13 15:51:35 +0100

    [ADD] Package files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyPostcode',
+  packages = ['pyPostcode'],
+  version = '0.1',
+  description = 'Request information about Dutch addresses from the PostcodeApi.nu API',
+  author = 'Stefan Jansen',
+  author_email = 'stefan@steffex.net',
+  url = 'https://github.com/steffex/pyPostcode',
+  download_url = 'https://github.com/steffex/pyPostcode/tarball/master',
+  keywords = ['postcodeapi.nu', 'zipcode', 'Netherlands', 'Dutch', 'localization'],
+  classifiers = [],
+)
